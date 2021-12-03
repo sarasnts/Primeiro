6bc2e6d80124792a8d98c08f8367a48a47a027d2
Sara Santos <sara2santoss@hotmail.com>
2021-12-03 19:40:59 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            var a = "hello ";
+            var b = "world!";
+            Console.WriteLine(a + b);
+
+        }
+    }
+
+}
